cbc589b4e90847eed10612601763b6abb5884965
ademola toukourou <tkradm@yahoo.fr>
2020-11-03 17:03:45 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :flats, only: [:new, :create]
+  resources :flats, only: [:new, :create, :show]
 end
