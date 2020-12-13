b97e50a6c66e9bf56e78b8cedb04b57fb3a054b7
ademola toukourou <tkradm@yahoo.fr>
2020-12-13 19:23:55 +0100

    style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @flats = Flat.all.order("created_at DESC").limit(4)
   end
 
   def myflats
